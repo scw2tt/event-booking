7cc4496d7b484597037ed06a7bff7b9fc624b349
scw2tt <scw2tt@virginia.edu>
2020-03-02 22:06:39 -0500

    added the requests, favicon, color to menu, home page get started tasks, other minor changes to site

--- event-booking/src/app/components/my-calendar/my-calendar.component.ts
+++ event-booking/src/app/components/my-calendar/my-calendar.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { CalendarEvent, CalendarView } from 'angular-calendar';
+import { colors } from '../my-calendar-utils/colors';
 
 @Component({
   selector: 'app-my-calendar',
+  //changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './my-calendar.component.html',
   styleUrls: ['./my-calendar.component.css']
 })
-export class MyCalendarComponent implements OnInit {
 
-  constructor() { }
+export class MyCalendarComponent {
+  view: CalendarView = CalendarView.Month;
 
-  ngOnInit(): void {
-  }
+  viewDate: Date = new Date();
 
+  events: CalendarEvent[] = [
+    {
+      title: 'Editable event',
+      color: colors.yellow,
+      start: new Date(),
+      actions: [
+        {
+          label: '<i class="fa fa-fw fa-pencil"></i>',
+          onClick: ({ event }: { event: CalendarEvent }): void => {
+            console.log('Edit event', event);
+          }
+        }
+      ]
+    },
+    {
+      title: 'Deletable event',
+      color: colors.blue,
+      start: new Date(),
+      actions: [
+        {
+          label: '<i class="fa fa-fw fa-times"></i>',
+          onClick: ({ event }: { event: CalendarEvent }): void => {
+            this.events = this.events.filter(iEvent => iEvent !== event);
+            console.log('Event deleted', event);
+          }
+        }
+      ]
+    },
+    {
+      title: 'Non editable and deletable event',
+      color: colors.red,
+      start: new Date()
+    }
+  ];
 }
