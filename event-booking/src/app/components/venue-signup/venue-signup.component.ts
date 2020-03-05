88c441bb38290c721d8fde8a1b3f9d6716cc2f2b
emussey <eem4dd@virginia.edu>
2020-03-05 07:54:56 -0500

    venue signup page

---
+++ event-booking/src/app/components/venue-signup/venue-signup.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-venue-signup',
+  templateUrl: './venue-signup.component.html',
+  styleUrls: ['./venue-signup.component.css']
+})
+export class VenueSignupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
