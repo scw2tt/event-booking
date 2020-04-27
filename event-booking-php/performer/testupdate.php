163e2994edb30d836d140dcbd36ab90bbd5ee463
emussey <eem4dd@virginia.edu>
2020-04-27 18:05:07 -0400

    I have no idea what these changes are and they are old

--- event-booking-php/performer/testupdate.php
+++ event-booking-php/performer/testupdate.php
@@ -4,7 +4,7 @@ require('performerHandler.php');
 
 
 //function editPerformer($about_me, $email, $genre, $link, $location, $name, $password)
-editPerformer("We are Paramore", "p@p.p", "indie", "http://paramore.net", "Nashville", "Paramore", "paramore");
+editPerformer("We are Paramore", "greenday@green.day", "indie", "http://paramore.net", "Nashville", "Green Day Bois", "12345");
 
 
  ?>
